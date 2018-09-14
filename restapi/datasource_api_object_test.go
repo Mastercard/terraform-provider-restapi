4efcbe0fd9b1b42bf4c41f14723f7bdb3dae4de1
Daniel Ruggeri <DRuggeri@primary.net>
2018-09-14 21:03:21 +0000

    Add working note about complex datasource test

--- restapi/datasource_api_object_test.go
+++ restapi/datasource_api_object_test.go
@@ -9,6 +9,7 @@ package restapi
 
 import (
   "os"
+  "fmt"
   "testing"
   "github.com/hashicorp/terraform/helper/resource"
   "github.com/Mastercard/terraform-provider-restapi/fakeserver"
@@ -19,26 +20,47 @@ func TestAccRestapiobject_Basic(t *testing.T) {
   api_server_objects := make(map[string]map[string]interface{})
 
   svr := fakeserver.NewFakeServer(8082, api_server_objects, true, debug)
-  if os.Getenv("REST_API_URI") != "http://127.0.0.1:8082" {
-    t.Fatalf("REST_API_URI environment variable must be set to 'http://127.0.0.1:8082' but it is set to '%s'", os.Getenv("REST_API_URI"))
-  }
+  os.Setenv("REST_API_URI", "http://127.0.0.1:8082")
 
   client, err := NewAPIClient("http://127.0.0.1:8082/", false, "", "", make(map[string]string, 0), 2, "id", make([]string, 0), false, false, debug)
   if err != nil { t.Fatal(err) }
-  client.send_request("POST", "/api/objects", `{ "id": "1234", "first": "Foo", "last": "Bar" }`)
+
+  /* Send a simple object */
+  client.send_request("POST", "/api/objects", `
+    {
+      "id": "1234",
+      "first": "Foo",
+      "last": "Bar"
+    }
+  `)
+
+  /* Send a complex object that we will pretend is the results of a search */
+  client.send_request("POST", "/api/objects", `
+    {
+      "id": "people",
+      "results": {
+        "number": 2,
+        "list": [
+          { "id": "1234", "first": "Foo", "last": "Bar" },
+          { "id": "4321", "first": "Foo", "last": "Baz" }
+        ]
+      }
+    }
+  `)
 
   resource.UnitTest(t, resource.TestCase{
     Providers:    testAccProviders,
     PreCheck:     func() { svr.StartInBackground() },
     Steps: []resource.TestStep{
       {
-        Config: `
+        Config: fmt.Sprintf(`
             data "restapi_object" "Foo" {
                path = "/api/objects"
                search_key = "first"
                search_value = "Foo"
+               debug = %t
             }
-          `,
+          `, debug),
         Check: resource.ComposeTestCheckFunc(
           testAccCheckRestapiObjectExists("data.restapi_object.Foo", "1234", client),
           resource.TestCheckResourceAttr("data.restapi_object.Foo", "id", "1234"),
@@ -46,6 +68,26 @@ func TestAccRestapiobject_Basic(t *testing.T) {
           resource.TestCheckResourceAttr("data.restapi_object.Foo", "api_data.last", "Bar"),
         ),
       },
+/* TODO: Fails with fakeserver because a request for /api/objects/people/4321 is unexpected (400 error)
+         Find a way to test this effectively
+      {
+        Config: fmt.Sprintf(`
+            data "restapi_object" "Baz" {
+               path = "/api/objects/people"
+               search_key = "last"
+               search_value = "Baz"
+               results_key = "results/list"
+               debug = %t
+            }
+          `, debug),
+        Check: resource.ComposeTestCheckFunc(
+          testAccCheckRestapiObjectExists("data.restapi_object.Baz", "4321", client),
+          resource.TestCheckResourceAttr("data.restapi_object.Baz", "id", "4321"),
+          resource.TestCheckResourceAttr("data.restapi_object.Baz", "api_data.first", "Foo"),
+          resource.TestCheckResourceAttr("data.restapi_object.Baz", "api_data.last", "Baz"),
+        ),
+      },
+*/
     },
   })
 
