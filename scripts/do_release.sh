1e2384593b261b15c61084bd64b0f2bf46ffcbce
Daniel Ruggeri <Daniel.Ruggeri@mastercard.com>
2021-09-06 14:57:38 -0500

    Update script to do all work of consequence in the action

--- scripts/do_release.sh
+++ scripts/do_release.sh
@@ -1,5 +1,59 @@
 #!/bin/bash -e
 
+usage (){
+echo "$0 - Tag and prepare a release
+USAGE: $0 (major|minor|patch|vX.Y.Z)
+The argument may be one of:
+major  - Increments the current major version and performs the release
+minor  - Increments the current minor version and preforms the release
+patch  - Increments the current patch version and preforms the release
+vX.Y.Z - Sets the tag to the value of vX.Y.Z where X=major, Y=minor, and Z=patch
+"
+  exit 1
+}
+
+if [ -z "$1" -o -n "$2" ];then
+  usage
+fi
+
+TAG=`git describe --tags --abbrev=0`
+VERSION="${TAG#[vV]}"
+MAJOR="${VERSION%%\.*}"
+MINOR="${VERSION#*.}"
+MINOR="${MINOR%.*}"
+PATCH="${VERSION##*.}"
+echo "Current tag: v$MAJOR.$MINOR.$PATCH"
+
+#Determine what the user wanted
+case $1 in
+  major)
+    MAJOR=$((MAJOR+1))
+    MINOR=0
+    PATCH=0
+    TAG="v$MAJOR.$MINOR.$PATCH"
+    ;;
+  minor)
+    MINOR=$((MINOR+1))
+    PATCH=0
+    TAG="v$MAJOR.$MINOR.$PATCH"
+    ;;
+  patch)
+    PATCH=$((PATCH+1))
+    TAG="v$MAJOR.$MINOR.$PATCH"
+    ;;
+  v*.*.*)
+    TAG="$1"
+    ;;
+  *.*.*)
+    TAG="v$1"
+    ;;
+  *)
+    usage
+    ;;
+esac
+
+echo "New tag: $TAG"
+
 #Build the docs first
 cd $(dirname $0)
 WORK_DIR=$(pwd)
@@ -12,99 +66,19 @@ if [ -n "$DIFFOUTPUT" ];then
   git push
 fi
 
-OSs=("darwin" "linux" "windows")
-ARCHs=("386" "amd64")
-
 export REST_API_URI="http://127.0.0.1:8082"
 [[ -z "${GOPATH}" ]] && export GOPATH=$HOME/go
 export CGO_ENABLED=0
 
-#Get into the right directory
+#Get into the right directory and build/test
 cd "$WORK_DIR"
-
-#Parse command line params
-CONFIG=$@
-for line in $CONFIG; do
-  eval "$line"
-done
-
-if [[ -z "$github_api_token" && -f github_api_token ]];then
-  github_api_token=$(cat github_api_token)
-fi
-
-if [[ -z "$owner" ]];then
-  owner="Mastercard"
-fi
-
-if [[ -z "$repo" ]];then
-  repo="terraform-provider-restapi"
-fi
-
-if [[ -z "$github_api_token" || -z "$owner" || -z "$repo" || -z "$tag" ]];then
-  echo "USAGE: $0 github_api_token=TOKEN owner=someone repo=somerepo tag=vX.Y.Z"
-  exit 1
-fi
-
-if [[ -z "$GPG_PRIVATE_KEY" || -z "$PASSPHRASE" ]];then
-  echo "ERROR: GPG_PRIVATE_KEY or PASSPHRASE env variable is not set"
-  exit 1
-fi
-
-if [[ "$tag" != v* ]];then
-  tag="v$tag"
-fi
-
-version=$(echo $tag | cut -c 2-)
-
 ./test.sh
+cd ../
 
-#Build for all architectures we want
-ARTIFACTS=()
+vi .release_info.md
 
-echo "Building..."
-for GOOS in "${OSs[@]}";do
-  for GOARCH in "${ARCHs[@]}";do
-    export GOOS GOARCH
+git commit -m "Changes for $TAG" .release_info.md
 
-    if [[ "$GOOS" = "windows" ]];then
-      EXTENSION=".exe"
-    else
-      EXTENSION=""
-    fi
-    ZIP_FILE="terraform-provider-restapi_${version}_${GOOS}_${GOARCH}.zip"
-    TF_OUT_FILE="terraform-provider-restapi_${tag}${EXTENSION}"
-    echo "  $GOOS - $GOARCH: $TF_OUT_FILE"
-    go build -o "$TF_OUT_FILE" ../
-    zip "$ZIP_FILE" "$TF_OUT_FILE"
-    rm -f "$TF_OUT_FILE"
-    ARTIFACTS+=("$ZIP_FILE")
-
-    FS_OUT_FILE="fakeserver_$tag-$GOOS-$GOARCH"
-    echo "  $FS_OUT_FILE"
-    go build -o "$FS_OUT_FILE" ../fakeservercli
-    ARTIFACTS+=("$FS_OUT_FILE")
-  done
-done
-
-shasum -a 256 *.zip > "terraform-provider-restapi_${version}_SHA256SUMS"
-ARTIFACTS+=("terraform-provider-restapi_${version}_SHA256SUMS")
-
-#### Signing
-export GNUPGHOME=tmpgpg
-rm -rf tmpgpg
-mkdir tmpgpg
-echo "$GPG_PRIVATE_KEY" | gpg --batch --import
-
-gpg --pinentry-mode=loopback --passphrase "$PASSPHRASE" --detach-sign "terraform-provider-restapi_${version}_SHA256SUMS"
-ARTIFACTS+=("terraform-provider-restapi_${version}_SHA256SUMS")
-
-#Create the release so we can add our files
-./create-github-release.sh github_api_token=$github_api_token owner=$owner repo=$repo tag=$tag draft=false
-
-#Upload all of the files to the release
-for FILE in "${ARTIFACTS[@]}";do
-  ./upload-github-release-asset.sh github_api_token=$github_api_token owner=$owner repo=$repo tag=$tag filename="$FILE"
-done
-
-echo "Cleaning up..."
-rm -f release_info.md terraform-provider-restapi_*.zip fakeserver-* *SHA256SUMS*
+git tag $TAG
+git push origin
+git push origin $TAG
