ac6f2cdf2b2fb1503e5cf859a212acf3893d38ed
Daniel Ruggeri <Daniel.Ruggeri@mastercard.com>
2022-06-27 23:04:33 +0000

    Convert main to terraform-plugin-sdk use

--- main.go
+++ main.go
@@ -2,13 +2,14 @@ package main
 
 import (
 	"github.com/Mastercard/terraform-provider-restapi/restapi"
+
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/plugin"
-	"github.com/hashicorp/terraform-plugin-sdk/v2/terraform"
 )
 
 func main() {
 	plugin.Serve(&plugin.ServeOpts{
-		ProviderFunc: func() terraform.ResourceProvider {
+		ProviderFunc: func() *schema.Provider {
 			return restapi.Provider()
 		},
 	})
