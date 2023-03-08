297f50e16926e5937bcc12ae4bdb1dbebe650814
Kenneth Bingham <w@qrk.us>
2023-03-08 08:14:13 -0500

    add debug mode

--- main.go
+++ main.go
@@ -1,6 +1,10 @@
 package main
 
 import (
+	"context"
+	"flag"
+	"log"
+
 	"github.com/Mastercard/terraform-provider-restapi/restapi"
 
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
@@ -8,9 +12,25 @@ import (
 )
 
 func main() {
-	plugin.Serve(&plugin.ServeOpts{
+
+	var debugMode bool
+	flag.BoolVar(&debugMode, "debug", false, "set to true to run the provider with support for debuggers like delve")
+	flag.Parse()
+
+	opts := &plugin.ServeOpts{
 		ProviderFunc: func() *schema.Provider {
 			return restapi.Provider()
 		},
-	})
+	}
+
+	if debugMode {
+		err := plugin.Debug(context.Background(), "terraform.netfoundry.io/openziti/restapi", opts)
+		if err != nil {
+			log.Fatal(err.Error())
+		}
+		return
+	}
+
+	plugin.Serve(opts)
+
 }
